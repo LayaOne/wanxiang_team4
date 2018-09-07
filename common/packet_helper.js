16cd15211e4d37ee8c840a00b68075250b6d52c5
Xhacker's New mac <17599228@qq.com>
2018-09-08 02:49:54 +0800

    packet helper

---
+++ common/packet_helper.js
@@ -0,0 +1,51 @@
+var PacketHelper = {};
+
+
+/**
+ * 生成公民
+ * @param {String} user_nickname 
+ */
+PacketHelper.generate_create_user = function(user_nickname){
+    var packet = {};
+    packet.cmd = 'user_nickname';
+    packet.data = {};
+    packet.data.user_nickname = user_nickname;
+    return packet;
+}
+
+PacketHelper.login_with_token = function(user_token){
+    var packet = {};
+    packet.cmd = 'user_login_with_token';
+    packet.data = {};
+    packet.data.user_token = user_token;
+    return packet;
+}
+
+/**
+ * 
+ * @param {*} bSuc 成功与否
+ * @param {*} user_token 用户token
+ * @param {*} need_confirm_number 等待confirm的合约
+ */
+PacketHelper.on_logined = function(bSuc, user_token){
+    var packet = {};
+    packet.cmd = 'logined';
+    packet.data = {};
+    if(bSuc == false){
+        packet.data.retcode = 1;
+        packet.data.msg = '登录失败'
+    }
+    else{
+        packet.data.retcode = 0;
+        packet.data.user_token = user_token;
+    }
+
+    return packet;
+}
+
+
+PacketHelper.generate_create_contract = function(user_id){
+
+}
+
+module.exports = PacketHelper;
---
+++ test/test_packet.js
@@ -0,0 +1,2 @@
+var p = require('../common/packet_helper');
+p.login_with_token('123');
