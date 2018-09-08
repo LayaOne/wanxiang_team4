0e2df286eee8ad646cc4511c6c8a8b1cc92add34
tyrande000 <tyrande000@gmail.com>
2018-09-08 21:46:54 +0800

    test

--- logic/wancloud/api.js
+++ logic/wancloud/api.js
@@ -3,27 +3,27 @@
  */
 const config = require("../../config");
 const request = require('superagent');
+const randomstring = require('randomstring');
 
 module.exports = (function(){
 	return new wancloud_api();
 })();
 
 function wancloud_api(){
-
+	this.map = {};
 }
-
+/*
 wancloud_api.prototype.set = function(raw_data, label){
-	/*
-	https://api.wancloud.io/apis/bcs/entry
-	{
-		"rawData": "123456789",
-		"label": {
-			"age": "12",
-			"size": "100",
-			"type": "a"
-		}
-	}
-	*/
+	
+	// https://api.wancloud.io/apis/bcs/entry
+	// {
+	// 	"rawData": "123456789",
+	// 	"label": {
+	// 		"age": "12",
+	// 		"size": "100",
+	// 		"type": "a"
+	// 	}
+	// }
 	
 	return new Promise((resolve, reject) => {
 		request
@@ -45,17 +45,17 @@ wancloud_api.prototype.set = function(raw_data, label){
 }
 
 wancloud_api.prototype.get = function(raw_data_hash){
-	/*
-	{
-		"rawData": "万向区块链",
-		"rawDataHash": "b0069eff1ac6795c58bf1dfbcb0de0ff",
-		"status": 1,
-		"label": {
-			"type": "bl",
-			"address": "上海"
-		}
-	}
-	*/
+	
+	// {
+	// 	"rawData": "万向区块链",
+	// 	"rawDataHash": "b0069eff1ac6795c58bf1dfbcb0de0ff",
+	// 	"status": 1,
+	// 	"label": {
+	// 		"type": "bl",
+	// 		"address": "上海"
+	// 	}
+	// }
+	
 	return new Promise((resolve, reject) => {
 		request
 		.get("https://api.wancloud.io/apis/bcs/entry/" + raw_data_hash)
@@ -68,4 +68,24 @@ wancloud_api.prototype.get = function(raw_data_hash){
 			resolve(res.body);
 		});
 	});
+}
+*/
+wancloud_api.prototype.set = function(raw_data, label){
+	return new Promise((resolve, reject) => {
+		let hash = randomstring.generate(32);
+
+		this.map[hash] = {
+			"rawData": raw_data,
+			"rawDataHash": hash,
+			"label": label
+		}
+
+		resolve(hash);
+	});	
+}
+
+wancloud_api.prototype.get = function(raw_data_hash){
+	return new Promise((resolve, reject) => {
+		resolve(this.map[raw_data_hash]);
+	});	
 }
