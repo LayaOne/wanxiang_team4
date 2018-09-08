98bd511a35ca6f530eb392075fb9733233247b62
tyrande000 <tyrande000@gmail.com>
2018-09-08 21:55:16 +0800

    test

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ var root = path.resolve(__dirname);
 var config = {
     "RELEASE" : process.env.NODE_ENV == "production",
     "MONGO_DB" : {
-		"URL" : "mongodb://127.0.0.1:3000/wanxiang_team4"
+		"URL" : "mongodb://127.0.0.1:3001/wanxiang_team4"
     }
 };
 
