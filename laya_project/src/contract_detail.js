289f60afa586357cb2ab5343fe9d4b55092c148a
xhacker5000 <17599228@qq.com>
2018-09-08 21:47:22 +0800

    localstroage

---
+++ laya_project/src/contract_detail.js
@@ -0,0 +1,12 @@
+var ContractDetailPage = function(){
+    ContractDetailPage.super(this);
+
+    ContractDetailPage.initlize = function(){
+        
+
+    }
+
+    this.initlize();
+}
+
+module.exports = ContractDetailPage;
--- laya_project/src/main_page.js
+++ laya_project/src/main_page.js
@@ -1,26 +1,55 @@
 
 var myDirect = require('./myDirector');
+var PacketCMD = require('../../common/packet_helper');
+
 var MainPage = function(){
     MainPage.super(this);
     var that = this;
     var ViewManager = require('./viewManager');
+    var SocketClient = myDirect.getSocketClient();
     MainPage.prototype.initlize = function(){
+
+
+        SocketClient.regist_callback('logined',)
 
         var self_pub = myDirect.get_pubkey();
         if(!self_pub){
-            this.check_my_public.visible = false;
+            var user_key = lcc.genNewUserInfo();
+            console.log(user_key)
+            myDirector.save_key(user_key.public,user_key.private);
+
             this.my_contract_list.visible = false;
         }
+
+        var self_token = myDirect.get_token();
+        if(!self_token){
+            //this.check_my_public.visible = false;
+            this.my_contract_list.visible = false;
+        }
+        else{
+
+            var packet = PacketCMD.login_with_token;
+        }
+
+
 
         
         //open public page
         this.check_my_public.on('click',null,function(){
-            ViewManager.show_public_page();
+
+            var pubkey = myDirect.get_pubkey();
+            ViewManager.show_public_page(pubkey);
         })
 
         //open contract
         this.create_contract_button.on('click',null,function(){
-            ViewManager.show_login_view();
+            if(!self_token){
+                ViewManager.show_login_view();
+            }
+            else{
+                ViewManager.show_create_page();
+            }
+            
         })
 
         this.my_contract_list.on('click',null,function(){
@@ -29,16 +58,26 @@ var MainPage = function(){
 
         this.change_langurage.on('click',null,function(){
             if(ViewManager.current_langurage == 'ENG'){
-                that.change_langurage.text = '中文';
+                that.change_langurage.text = 'ENG';
                 ViewManager.set_langurage('CHN')
 
             }
             else{
+                that.change_langurage.text = '中文';
                 ViewManager.set_langurage('ENG');
-                that.change_langurage.text = 'ENG';
+                
             }
             
         })
+    }
+
+
+    MainPage.prototype.recv_logined = function(data){
+        if(data.retcode == 0){
+            myDirect.save_token(data.token);
+            myDirect.user_nickname = data.user_nickname;
+            myDirect.reputation = data.reputation;
+        }
     }
 
     this.initlize();
